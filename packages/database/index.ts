d7e97619d23a8eadaadda84a292ccba00bbfa99a
Cristian Freitas <crismdf2003@gmail.com>
2025-12-01 13:16:44 -0300

    feat: Introduce `env` package for environment variable validation and `database` package with Prisma setup and initial schema.

---
+++ packages/database/index.ts
@@ -0,0 +1,8 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { env } from '@triae/env'
+import { PrismaClient } from './generated/prisma/client'
+
+const adapterPostgres = new PrismaPg(env.DATABASE_URL)
+const prismaClient = new PrismaClient({ adapter: adapterPostgres })
+
+export { prismaClient as prisma }
